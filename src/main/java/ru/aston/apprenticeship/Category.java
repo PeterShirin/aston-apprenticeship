1a29146aeab33ad044fb241a95b305f8ac820bfa
Petr Shirin <petrshirin91@gmail.com>
2023-11-16 13:28:30 +0400

    create entities: product, category, subcategory

---
+++ src/main/java/ru/aston/apprenticeship/Category.java
@@ -0,0 +1,42 @@
+package ru.aston.apprenticeship;
+
+import java.util.List;
+
+public class Category {
+    private int id;
+    private String name;
+    private List<Product> products;
+    private List<Subcategory> subcategories;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Subcategory> getSubcategories() {
+        return subcategories;
+    }
+
+    public void setSubcategories(List<Subcategory> subcategories) {
+        this.subcategories = subcategories;
+    }
+}
